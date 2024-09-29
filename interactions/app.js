168e95a35a1a166f2cafaad0591c69cc8861e7de
zefrenchwan <cenalon@yahoo.fr>
2024-09-29 08:50:23 +0200

    add session base for interactions

--- interactions/app.js
+++ interactions/app.js
@@ -1,12 +1,24 @@
-const server = require('fastify')({ logger: true })
+const fastify = require('fastify');
+const fastifySession = require('@fastify/session');
+const fastifyCookie = require('@fastify/cookie');
+
+// general properties, docker will allow changes
 const port = 3000
 const addr = '0.0.0.0'
+const session_secret = process.env.SESSION_SECRET
 
-server.get('/', (_request, reply) => {
+// configuring app
+const app = fastify();
+app.register(fastifyCookie);
+app.register(fastifySession, {secret: session_secret});
+
+// adding routes
+app.get('/', (_request, reply) => {
    reply.status(200).send("Hello World");
 });
 
-server.listen({host: addr, port: port}, error => {
+// launching server
+app.listen({host: addr, port: port}, error => {
   if (error) {
     process.exit(1);
   }
