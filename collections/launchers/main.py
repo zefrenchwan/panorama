3849f773b44a2c6b483351bd6eb517fc86448f10
zefrenchwan <cenalon@yahoo.fr>
2024-10-07 08:39:40 +0200

    fixing kafka boot

--- collections/launchers/main.py
+++ collections/launchers/main.py
@@ -1,7 +1,10 @@
 from bottle import route, run
 from psycopg import connect
 from psycopg.rows import dict_row
+from kafka import KafkaProducer
 from os import getenv
+from uuid import uuid4
+from time import time
 
 
 def load_tasks_to_run() -> list[dict[str,str]]:
@@ -25,6 +28,13 @@ def load_tasks_to_run() -> list[dict[str,str]]:
 def launch():
     tasks = load_tasks_to_run()
     size = len(tasks)
+    bootstrap_url = getenv('MESSAGES_URL')
+    producer = KafkaProducer(bootstrap_servers=bootstrap_url)
+    for task in tasks:
+        task_id = str(uuid4())
+        topic = task["task_processor"]
+        now = round(time() * 1000)
+        producer.send(topic = topic, value='test',key=task_id,  timestamp_ms=now)
     return f"Should launch {size} tasks"
 
 
