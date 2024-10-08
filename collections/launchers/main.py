f6592d49112450ec721d4c1c188ff6ea5964b075
zefrenchwan <cenalon@yahoo.fr>
2024-10-08 19:27:19 +0200

    both producer and consumer cooperate through kafka

--- collections/launchers/main.py
+++ collections/launchers/main.py
@@ -5,9 +5,24 @@ from kafka import KafkaProducer
 from os import getenv
 from uuid import uuid4
 from time import time
+from json import dumps
+import logging
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+logger = logging.getLogger(__name__)
+
+# TODO: remove this
+fake_data = True
 
 def load_tasks_to_run() -> list[dict[str,str]]:
+    if fake_data:
+        return [{"task_processor": "stdcollector", "task_url": "https://www.google.fr", "task_keywords": "work harder, you dumbass"}]
     url = getenv('DBCONF_URL')
     result = list()
     with connect(url, row_factory=dict_row) as connection:
@@ -29,12 +44,18 @@ def launch():
     tasks = load_tasks_to_run()
     size = len(tasks)
     bootstrap_url = getenv('MESSAGES_URL')
-    producer = KafkaProducer(bootstrap_servers=bootstrap_url)
+    producer = KafkaProducer(bootstrap_servers=bootstrap_url, key_serializer = lambda k: k.encode('utf-8'), value_serializer = lambda v: v.encode('utf-8'))
     for task in tasks:
         task_id = str(uuid4())
         topic = task["task_processor"]
         now = round(time() * 1000)
-        producer.send(topic = topic, value='test',key=task_id,  timestamp_ms=now)
+        task_url = task.get("task_url")
+        task_keywords = task.get("task_keywords")
+        if task_url is None and task_keywords is None:
+            logger.error(f"cannot process message, neither keywords nor url" + str(task))
+        else:
+            payload = dumps({"task_id": task_id, "source": task_url, "keywords": task_keywords})           
+            producer.send(topic = topic, value=payload,key=task_id,  timestamp_ms=now)
     return f"Should launch {size} tasks"
 
 
@@ -46,4 +67,5 @@ def simulate():
 
 
 if __name__ == '__main__':
+    logger.info("STARTING LAUNCH APP")
     run(host='0.0.0.0', port=8080)
--- collections/stdcollector/main.py
+++ collections/stdcollector/main.py
@@ -1,11 +1,20 @@
 from os import getenv
 from kafka import KafkaConsumer
+from json import loads
+import logging 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
+logger = logging.getLogger(__name__)
 
 topic_name = getenv('TOPIC_NAME')
 bootstrap_url = getenv('MESSAGES_URL')
-consumer = KafkaConsumer(topic_name,
-                         group_id='stdcollector',
-                         bootstrap_servers=[bootstrap_url])
+consumer = KafkaConsumer(topic_name, bootstrap_servers=[bootstrap_url],value_deserializer=lambda message: loads(message.decode('utf-8')))
+
 
 for message in consumer:
-    print(message)
+    logger.info(message)
